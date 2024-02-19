51d690526a7d975571ae9e2ec80b92fadbaeafb8
Um6253 <um6253@srmist.edu.in>
2024-02-19 20:55:35 +0530

    [Mani]Adding:Applying inner joint to different tables.

--- SQL4.sql
+++ SQL4.sql
@@ -8,3 +8,27 @@ alter table emp2 drop constraint DF__emp2__age__6EF57B66;
 alter table emp2 alter column empname varchar(30);
 alter table emp2 alter column empname char(30);
 select empid as 'employ id', depid as 'departmwnt id',empname as'employ name' from emp2;
+create table emp
+(
+  empid varchar (50) primary key,
+empname varchar(60)not null,
+empsalary decimal(10,2),
+depid integer
+);
+alter table emp drop constraint PK__emp__AF4CE865A3594E02;
+alter table emp alter column depid varchar(10);
+insert into emp values(111,'mani',100000,'d1');
+insert into emp values(112,'nani',150000,'d2');
+insert into emp values(113,'mom',200000,'d3');
+insert into emp values(114,'dad',250000,'d1');
+insert into emp values(115,'mohan',300000,'d2');
+insert into emp values(116,'vidya',350000,'d3');
+select * from emp;
+select * from dep;
+create table dep(depid varchar(10),depname varchar(20),deploc varchar(30));
+insert into dep values('d1','dev','hyd');
+insert into dep values('d2','test','mumbai');
+insert into dep values('d3','data','chennai');
+insert into dep values('d5','data','hyd');
+
+select e.empid,e.empname,e.empsalary,d.depid,d.deploc from emp e inner join dep d on e.depid=d.depid;
