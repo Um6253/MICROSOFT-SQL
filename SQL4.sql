21a5dc225bc275c150e0e8a0086d8c56afd88859
Um6253 <um6253@srmist.edu.in>
2024-02-19 21:25:52 +0530

    [Mani]Adding:Applying full outer joint to different tables.

--- SQL4.sql
+++ SQL4.sql
@@ -40,3 +40,6 @@ emp e left outer join dep d on e.depid=d.depid;
 
 select e.empid,e.depid,.empname,e.empsalary,d.depid,d.deploc from 
 emp e right outer join dep d on e.depid=d.depid;
+select e.empid,e.empname,e.empsalary,d.depid,d.deploc from 
+emp e full outer join dep d on e.depid=d.depid;
+
