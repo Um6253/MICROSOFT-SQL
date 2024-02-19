7a2c5efe88b677b1a9d2ce7ea52c7fc82877b734
Um6253 <um6253@srmist.edu.in>
2024-02-19 16:54:36 +0530

    [Mani]Adding:Adding Aliasing to column names.

--- SQL4.sql
+++ SQL4.sql
@@ -5,3 +5,6 @@ Alter table emp2 Add age int NOT NULL default 31;
 alter table emp2 drop constraint  DF__emp2__columnage__6E01572D;
 alter table emp2 drop column empage;
 alter table emp2 drop constraint DF__emp2__age__6EF57B66;
+alter table emp2 alter column empname varchar(30);
+alter table emp2 alter column empname char(30);
+select empid as 'employ id', depid as 'departmwnt id',empname as'employ name' from emp2;
